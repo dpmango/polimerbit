0e5e54f542b9577e083ec1b8e1b76b7285b26828
max197a <max197aa@gmail.com>
2019-09-11 19:47:36 +0300

    fixes

--- src/components/brands/brands.js
+++ src/components/brands/brands.js
@@ -5,20 +5,20 @@
   APP.Components.Brands = {
     init: function() {
       $(document).on('mouseenter', '[js-hover-polimer]', function() {
-        $('.brands__bg--polimer').fadeIn();
+        $('.brands__bg--polimer').addClass('is-active');
         $('.brands').addClass('brands--hover');
       });
       $(document).on('mouseleave', '[js-hover-polimer]', function() {
-        $('.brands__bg--polimer').fadeOut();
+        $('.brands__bg--polimer').removeClass('is-active');
         $('.brands').removeClass('brands--hover');
       });
 
       $(document).on('mouseenter', '[js-hover-forma]', function() {
-        $('.brands__bg--forma').fadeIn();
+        $('.brands__bg--forma').addClass('is-active');
         $('.brands').addClass('brands--hover');
       });
       $(document).on('mouseleave', '[js-hover-forma]', function() {
-        $('.brands__bg--forma').fadeOut();
+        $('.brands__bg--forma').removeClass('is-active');
         $('.brands').removeClass('brands--hover');
       });
     },
